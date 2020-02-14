11bc25f1c1d98bd8ad3b3db92170d2e1381d2250
Viduth Tennakoon <viduth.vh@gmail.com>
2020-02-14 11:48:55 +0530

    features
    Adding LI and UL through js,

--- client/html/js/features.js
+++ client/html/js/features.js
@@ -17,9 +17,19 @@ async function onfeatureLoad(){
     var feature1 = result[0];
     var feature2 = result[1];
     var feature3 = result [2];
+
     console.log(feature1);
     console.log(feature2);
-    console.log(feature3);
+    
+    var featureList = document.getElementById("featurelist");
+    for ( var i =0 ; i < result.length; i++){
+        var lieliment = document.createElement("li");
+        lieliment.innerHTML = result[i].body + " " + '<span class = "badge badge-success">' + result[i].author + ", " + result[i].time + "</span";  
+        
+        featureList.appendChild(lieliment);
+        lieliment.className= "list-group-item";
+
+    }
 }
 
 onfeatureLoad();
